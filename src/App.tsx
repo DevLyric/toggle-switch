72617f83934c3d97ab843bb60f61d68a33d9c232
devLyric <lyricsan@hotmail.com>
2023-11-28 12:21:35 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div className="container">
+            <label className="switch">
+                <input type="checkbox" />
+                <span className="slider round"></span>
+            </label>
+        </div>
+    );
+}
+
+export default App;
